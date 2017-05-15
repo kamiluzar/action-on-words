46866b5683f38942d8cdec8d25347eb7ef2763d8
kamiluzar <kamil.uzar@gmail.com>
2017-05-15 23:47:29 +0200

    0.0.1
    Added a function to check if a given word is a formal word.

--- src/main/java/com/kzr/controller/WordsController.java
+++ src/main/java/com/kzr/controller/WordsController.java
@@ -1,11 +1,14 @@
 package com.kzr.controller;
 
 import com.kzr.service.DictionarySerivce;
+import com.kzr.service.InformalWordsService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.net.MalformedURLException;
 
 /**
  * Created by Kamil on 2017-04-26.
@@ -15,9 +18,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class WordsController {
 
     private final DictionarySerivce dictionarySerivce;
+    private final InformalWordsService informalWordsService;
 
-    public WordsController(DictionarySerivce dictionarySerivce) {
+    public WordsController(DictionarySerivce dictionarySerivce, InformalWordsService informalWordsService) {
         this.dictionarySerivce = dictionarySerivce;
+        this.informalWordsService = informalWordsService;
     }
 
     @RequestMapping("/")
@@ -30,6 +35,13 @@ public class WordsController {
     String result = dictionarySerivce.findAntonyms(word);
     model.addAttribute("result", result);
     model.addAttribute("word", word);
+        return "home";
+    }
+
+    @RequestMapping(value = "/formal-list", method = RequestMethod.POST)
+    public String checkFormal (@RequestParam(value = "message") String text, Model model) throws MalformedURLException {
+        String result = informalWordsService.isFormal(text);
+        model.addAttribute("result2", result);
         return "home";
     }
 }
